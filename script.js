2a3e62cd0619898f7ce9c109e7d183bd74deba98
Mohamed Abubakar Siddiq <siddiq.h0@gmail.com>
2026-06-22 11:20:54 +0530

    Implement typing effect for portfolio words
    Added typing effect for displaying words dynamically.

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 console.log("Portfolio Loaded Successfully");
+const words = [
+"Software Tester",
+"QA Engineer",
+"Selenium Automation Tester",
+"Manual Testing Specialist"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+
+const typing = document.getElementById("typing");
+
+function typeEffect(){
+
+if(charIndex < words[wordIndex].length){
+
+typing.textContent += words[wordIndex].charAt(charIndex);
+
+charIndex++;
+
+setTimeout(typeEffect,100);
+
+}else{
+
+setTimeout(eraseEffect,1500);
+
+}
+}
+
+function eraseEffect(){
+
+if(charIndex > 0){
+
+typing.textContent =
+words[wordIndex].substring(0,charIndex-1);
+
+charIndex--;
+
+setTimeout(eraseEffect,50);
+
+}else{
+
+wordIndex++;
+
+if(wordIndex >= words.length){
+wordIndex = 0;
+}
+
+setTimeout(typeEffect,500);
+
+}
+}
+
+typeEffect();
